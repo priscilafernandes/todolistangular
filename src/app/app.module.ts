5e07cabbdfe7221a6832adc32e0b559babab8bbe
Marcilio <marcilio@Marcilios-iMac.local>
2020-04-20 14:04:39 -0300

    Criando estrutura da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TextComponent } from './text/text.component';
+import { MatInputModule } from '@angular/material/input';
+import { CardsContainerComponent } from './cards-container/cards-container.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CardComponent } from './card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    CardsContainerComponent,
+    CardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
