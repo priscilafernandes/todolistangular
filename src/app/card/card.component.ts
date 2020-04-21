e9a788ccf2ed84598e0a9332b062d1ee5a7d321b
priscilafernandes <pris1@outlook.com.br>
2020-04-21 10:53:26 -0300

    estilizando app-container e componente de input + botão

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  tesks:string[] = []
+  tasks: string[] = []
 
   constructor() { }
 
